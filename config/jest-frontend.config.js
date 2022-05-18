4a786a5066e189d28a3e004f1c6c1559b325ce79
Vikrant Kumar Sinha <96419531+VikrantKS@users.noreply.github.com>
2022-05-18 21:40:24 +0530

    Update jest-frontend.config.js

--- config/jest-frontend.config.js
+++ config/jest-frontend.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "rootDir": "..",
     "testRegex": "./test/frontend.spec.js",
     "transform": {
-      "^.+\\.jsx?$": "./path/to/wrapper.js"
+      "^.+\\.jsx?$": "./babel.config.js"
     },
 };
 
